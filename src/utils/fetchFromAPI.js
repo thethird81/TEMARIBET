ff9d78999e1f11e39ff4e91ff58646574462d4ad
thethird81 <thethird81@gmail.com>
2023-10-06 16:55:20 +0300

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+
+  headers: {
+    'X-RapidAPI-Key':'41b9b897ffmshfefbfdd58e3f4c6p1b704ajsnf1942cd13750',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
