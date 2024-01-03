d30496486d80a8857aed0994bd1d7bae6a62f427
thethird81 <thethird81@gmail.com>
2024-01-03 12:23:50 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box ,Stack} from '@mui/material';
 import { ChannelDetail, VideoDetail, SearchFeed,
-  Navbar, Feed, QuestionForm,AudioFileList,FirebaseAudio,
-  QuizSiglePage,QuizeList } from './components';
-import { alignProperty } from "@mui/material/styles/cssUtils";
-import { AlignHorizontalCenter } from "@mui/icons-material";
+        Navbar, Feed, QuestionForm,AudioFileList,
+        QuizSiglePage,QuizeList } from './components';
+
 
 
 
